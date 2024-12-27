7743dcd849e7d016d4960500f4482ac2193fcb7f
Hrishabh Mittal <hrishabhmittal.hm@gmail.com>
2024-12-27 16:42:08 +0530

    added some more stuff

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"golang.org/x/term"
 )
 
@@ -36,7 +37,13 @@ func getKey() (char byte,code byte) {
     }
     return
 }
-func editFile(data [][]byte,curpos []int,b bool) ([][]byte,[]int,bool) {
+var savepos int = 0
+func editFile(data [][]byte,curpos []int,offset []int,b bool) ([][]byte,[]int,[]int,bool) {
+    fd:=int(os.Stdin.Fd())
+    w,h,err:=term.GetSize(fd)
+    if err!=nil {
+        panic(err)
+    }
     char,code:=getKey()
     if char=='-' {
         b=false
@@ -50,11 +57,14 @@ func editFile(data [][]byte,curpos []int,b bool) ([][]byte,[]int,bool) {
                 break
             case ARROW_RIGHT:
                 curpos[1]++
+                savepos=curpos[1]
                 break
             case ARROW_LEFT:
                 curpos[1]--
+                savepos=curpos[1]
                 break
         }
+        curpos[1]=savepos
         if curpos[0]<0 {
             curpos[0]=0 
         } else if curpos[0]>len(data)-1 {
@@ -64,6 +74,16 @@ func editFile(data [][]byte,curpos []int,b bool) ([][]byte,[]int,bool) {
             curpos[1]=0 
         } else if curpos[1]>len(data[curpos[0]]) {
             curpos[1]=len(data[curpos[0]])
+        }
+        if curpos[0]<offset[0] {
+            offset[0]=curpos[0]
+        } else if curpos[0]>offset[0]+h-4 {
+            offset[0]=curpos[0]-h+4
+        }
+        if curpos[1]<offset[1] {
+            offset[1]=curpos[1]
+        } else if curpos[1]>offset[1]+w-4 {
+            offset[1]=curpos[1]-w+4
         }
     } else if char==127 {
         if curpos[1]==0 {
@@ -98,23 +118,36 @@ func editFile(data [][]byte,curpos []int,b bool) ([][]byte,[]int,bool) {
         data[curpos[0]] = append(line, data[curpos[0]][curpos[1]:]...)
         curpos[1]++
     }
-    return data,curpos,b
+    return data,curpos,offset,b
 }
-func Print(b [][]byte,cur []int) {
-    fmt.Print("\033[H\033[J")
-    for _,i:=range b {
-        fmt.Printf("%s\n",i)
+func Print(b [][]byte,cur []int,offset []int) {
+    fd:=int(os.Stdin.Fd())
+    w,h,err:=term.GetSize(fd)
+    if err!=nil {
+        panic(err)
     }
-    moveTo(cur[0],cur[1])
+    fmt.Print("\033[H\033[J")
+    for i:=offset[0];i<len(b)&&i<offset[0]+h;i++ {
+        if offset[1]>=len(b[i]) {
+            fmt.Printf("\n")
+        } else if offset[1]+w>len(b[i]) {
+            fmt.Printf("%s\n",b[i][offset[1]:])
+        } else {
+            fmt.Printf("%s\n",b[i][offset[1]:offset[1]+w])
+        }
+    }
+    moveTo(cur[0]-offset[0],cur[1]-offset[1])
 }
 func WriteFile(filename string,data [][]byte) {
     file, err := os.Create(filename)
     if err != nil {
         panic(err)
     }
-    for _,i:= range data {
+    for x,i:= range data {
         file.Write(i)
-        file.Write([]byte{'\n'})
+        if x!=len(data)-1 {
+            file.Write([]byte{'\n'})
+        }
     }
 }
 func ReadFile(fileName string) (data [][]byte) {
@@ -142,11 +175,14 @@ func main() {
     }
     d:=ReadFile(args[1])
     cur:=[]int{0,0}
+    offset:=[]int{0,0}
     editing:=true
     for editing {
-        Print(d,cur)
-        d,cur,editing=editFile(d,cur,editing)
+        Print(d,cur,offset)
+        d,cur,offset,editing=editFile(d,cur,offset,editing)
     }
     WriteFile(args[1],d)
     fmt.Print("\033[H\033[J")
 }
+
+
